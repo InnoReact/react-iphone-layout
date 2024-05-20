0347b3d4de0f09d3bc37bdb4dddaa3766ed90077
BangDori <qudwns8865@naver.com>
2024-05-21 00:10:21 +0900

    feat: 라이브러리 소개 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import ReactDOM from "react-dom/client";
 
 import { IPhoneLayout } from "./lib";
+import "./main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <IPhoneLayout isStatusBar isDynamicSize={true}>
-    <p>This is iPhone Layout!</p>
+    <div className="description">
+      <p className="message">
+        📱 This is dynamic iPhone layout!
+        <br />
+        Add your component here.
+      </p>
+      <p className="info">
+        <span className="label">More information:</span>
+        <a href="https://github.com/BangDori/react-iphone-layout">
+          react-iphone-layout
+        </a>
+      </p>
+    </div>
   </IPhoneLayout>
 );
