a8fb97e872195499ae35a27faeb1e8b5d3bb00d6
BangDori <qudwns8865@naver.com>
2024-05-25 15:12:12 +0900

    feat: npm link 추가

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -15,6 +15,14 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <fieldset className="ril-fieldset">
           <legend className="ril-legend">More Information</legend>
           <a
+            href="https://www.npmjs.com/package/react-iphone-layout"
+            className="github-link"
+            target="_blank"
+          >
+            Visit NPM
+          </a>
+          <br />
+          <a
             href="https://github.com/BangDori/react-iphone-layout"
             className="github-link"
             target="_blank"
