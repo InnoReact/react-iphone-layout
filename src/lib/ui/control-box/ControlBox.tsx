a24f1d4638309f4a4cbe555d941a1fc66fd07bb0
BangDori <44726494+BangDori@users.noreply.github.com>
2024-05-28 16:49:10 +0900

    update v1.1.0 (#15)
    * chore: svgr library
    * feat: update version 1.1.0
    * chore: 이미지 복사
    * chore: build formats 수정
    * chore: add start scripts
    * chore: yarn cache clean 추가
    * chore: exclude playground
    * chore: npmignore 수정
    * feat: gitignore 수정
    * feat: index.html 추가

---
+++ src/lib/ui/control-box/ControlBox.tsx
@@ -0,0 +1,77 @@
+import IPhoneIcon from "./assets/iPhone.svg?react";
+import LaptopIcon from "./assets/laptop.svg?react";
+import PlusIcon from "./assets/plus.svg?react";
+import MinusIcon from "./assets/minus.svg?react";
+
+import "./ControlBox.css";
+import "./Position.css";
+import { useResize } from "./hooks/useResize";
+
+interface ControlBoxProps {
+  position: "top" | "right" | "bottom" | "left";
+  mode: "iPhone" | "laptop";
+  iPhoneLayoutRef: React.RefObject<HTMLDivElement>;
+  iPhoneSizeRef: React.MutableRefObject<number>;
+  minSize: number;
+  maxSize: number;
+  handleChangeMode: (newMode: "iPhone" | "laptop") => void;
+}
+
+export const ControlBox: React.FC<ControlBoxProps> = ({
+  position,
+  mode,
+  iPhoneLayoutRef,
+  iPhoneSizeRef,
+  minSize,
+  maxSize,
+  handleChangeMode,
+}) => {
+  const { handleSizeUp, handleSizeDown } = useResize(
+    iPhoneLayoutRef,
+    iPhoneSizeRef,
+    minSize,
+    maxSize
+  );
+
+  if (mode === "laptop") {
+    return (
+      <div className="laptop__mode">
+        <button
+          className="iPhone-active-btn"
+          onClick={() => handleChangeMode("iPhone")}
+        >
+          <IPhoneIcon className="iphone-icon" width="64" height="64" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <aside className={`ril-control-box position__${position}`}>
+      <ul className="ril-control-list">
+        <li
+          className="ril-control-item"
+          onClick={() => handleChangeMode("iPhone")}
+        >
+          <IPhoneIcon
+            className={`iphone-icon ${
+              mode === "iPhone" ? "iPhone_focus_mode" : ""
+            }`}
+          />
+        </li>
+        <li
+          className="ril-control-item"
+          onClick={() => handleChangeMode("laptop")}
+        >
+          <LaptopIcon className="laptop-icon" />
+        </li>
+        <li className="ril-control-item" onClick={handleSizeUp}>
+          <PlusIcon className="plus-icon" />
+        </li>
+        <li className="ril-control-item" onClick={handleSizeDown}>
+          <MinusIcon className="minus-icon" />
+        </li>
+      </ul>
+    </aside>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     dts({ insertTypesEntry: true, tsconfigPath: "./tsconfig.json" }),
+    svgr({ include: "**/*.svg?react" }),
   ],
   build: {
     cssMinify: "esbuild",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "React-iPhone-Layout",
-      fileName: (format) => `iphone-layout.${format}.js`,
+      fileName: (format) => `react-iphone-layout.${format}.js`,
       formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "playground/**"],
       output: {
         assetFileNames: (assetInfo) => {
           if (assetInfo.name === "style.css") return "ReactIPhoneLayout.css";
